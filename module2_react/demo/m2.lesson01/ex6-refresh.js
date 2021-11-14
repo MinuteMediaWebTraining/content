81f7c37a8a30a8f9f7b283d5f6c49244da338472
yuvalbl <yuval.bar.levi@gmail.com>
2021-11-14 14:26:05 +0200

    m2.1

---
+++ module2_react/demo/m2.lesson01/ex6-refresh.js
@@ -0,0 +1,12 @@
+function math(a, b, type) {
+  return 0
+}
+
+console.log(math(1, 2, { type: 'add' }) === 3)
+console.log(math(1, 2, { type: 'sub' }) === -1)
+console.log(math(1, 2, { type: 'foo' }) === 0)
+console.log(math(1, 2, { typo: 'sub' }) === 3)
+console.log(math(1, 2, {}) === 3)
+console.log(math(1, 2) === 3)
+console.log(math(1) === 1)
+console.log(math() === 0)
